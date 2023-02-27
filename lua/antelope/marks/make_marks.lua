e8e21fff916877c630eb2919449cc65a830f74c3
Pheon-Dev <devpheon@gmail.com>
2023-02-27 15:42:09 +0300

    fix: make marks error

--- lua/antelope/marks/make_marks.lua
+++ lua/antelope/marks/make_marks.lua
@@ -1,25 +1,25 @@
-local Mark = require("reach.marks.mark")
+local Mark = require('antelope.marks.mark')
 
 return function(options)
-	local output = vim.api.nvim_exec("marks", true)
-	local rows = vim.split(output, "\n", { trimempty = true })
+  local output = vim.api.nvim_exec('marks', true)
+  local rows = vim.split(output, '\n', { trimempty = true })
 
-	table.remove(rows, 1)
+  table.remove(rows, 1)
 
-	local marks = {}
+  local marks = {}
 
-	for _, row in pairs(rows) do
-		local parts = vim.split(row, "[ \t]+", { trimempty = true })
-		local mark, line, col = unpack(parts, 1, 3)
+  for _, row in pairs(rows) do
+    local parts = vim.split(row, '[ \t]+', { trimempty = true })
+    local mark, line, col = unpack(parts, 1, 3)
 
-		table.insert(marks, Mark:new(mark, line, col, table.concat(parts, " ", 4)))
-	end
+    table.insert(marks, Mark:new(mark, line, col, table.concat(parts, ' ', 4)))
+  end
 
-	if type(options.filter) == "function" then
-		return vim.tbl_filter(function(m)
-			return options.filter(m.mark)
-		end, marks)
-	end
+  if type(options.filter) == 'function' then
+    return vim.tbl_filter(function(m)
+      return options.filter(m.mark)
+    end, marks)
+  end
 
-	return marks
+  return marks
 end
