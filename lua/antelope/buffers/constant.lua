2024bdceddab4b5dd6377b8cf4b29bfc2c96ecca
Pheon-Dev <devpheon@gmail.com>
2023-07-07 11:05:23 +0300

    [~] revert to old constants

--- lua/antelope/buffers/constant.lua
+++ lua/antelope/buffers/constant.lua
@@ -1,6 +1,6 @@
 local module = {}
 
 module.auto_handles =
-  vim.split('hjklfdsagbvncmuioewrpqtyxzABCDEFGHIJKLMNOPQRSTUVWXYZ!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~', '')
+  vim.split('123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~', '')
 
 return module
